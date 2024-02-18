77956965045fcbb3d0827da7b06a6c25e159362d
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-02-18 15:35:49 +0530

    creating home page header and local search icon for the home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,28 @@
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      Home Page
+    <>
+    <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+      <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+      <Link href='/ask-questions' className="flex justify-end max-sm:wfull">
+        <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">Ask a Question</Button>
+      </Link>
     </div>
+    <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+      <LocalSearch 
+        route='/' 
+        iconPosition='left'  
+        imgSrc='/assets/icons/search.svg'
+        placeholder='Search for questions'
+        otherClasses='flex-1'
+      />
+      Filters
+    </div>
+    </>
   );
 };
 
