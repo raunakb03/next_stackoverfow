dae0eb5119b9475c964bec57e23f51633757c336
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-02-18 16:19:34 +0530

    create filter for mobile screens

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -13,12 +13,13 @@ const LeftSidebar = () => {
   return (
     <section className="background-light900_dark200 light-border sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
-        {sidebarLinks.map((item) => {
+        {sidebarLinks.map((item, index) => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
             pathname === item.route;
           return (
             <Link
+              key={index}
               href={item.route}
               className={`${
                 isActive
