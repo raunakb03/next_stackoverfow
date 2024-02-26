4e7a81028965c4adaf5fbcc65117153ca58acf2e
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-02-26 19:11:24 +0530

    create no result component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,34 @@
 import Filter from "@/components/Filter";
+import NoResult from "@/components/NoResult";
 import HomeFilters from "@/components/home/HomeFilters";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
+
+const questions= [
+  {
+    _id: 1,
+    title: "How to use React Router with the latest version of react?",
+    tags: [{_id: 1, name: 'react'}, {_id: 2, name: 'javascript'}],
+    author: 'John Doe',
+    upvotes: 10,
+    views: 20,
+    answers: 5,
+    createdAt: '2021-09-01T00:00:00.000Z'
+  },
+  {
+    _id: 1,
+    title: "How to use React Router with the latest version of react?",
+    tags: [{_id: 1, name: 'react'}, {_id: 2, name: 'javascript'}],
+    author: 'John Doe',
+    upvotes: 10,
+    views: 20,
+    answers: 5,
+    createdAt: '2021-09-01T00:00:00.000Z'
+  },
+]
 
 const Home = () => {
   return (
@@ -23,16 +47,26 @@ const Home = () => {
           placeholder='Search for questions'
           otherClasses='flex-1'
         />
-        {/* <Filter 
-          filters={HomePageFilters}
-          otherClasses='min-h-[56px] sm:min-w-[170px]'
-          containerClasses='hidden max-md:flex'
-        /> */}
         <Filter 
           filters={HomePageFilters}
           otherClasses='min-h-[56px] sm:min-w-[170px]'
+          containerClasses='hidden max-md:flex'
         />
-        {/* <HomeFilters /> */}
+      </div>
+      <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {
+          questions.length > 0 ?
+          questions.map((question, index)=> (
+            'Qustion card'
+          )) : 
+          <NoResult
+            title= "There's no question to show"
+            description= "Be the first to beark the silence! Ask a question and kickstart the discussion. Our query coudl be the next big thing others learn from. Get involved!"
+            link= '/ask-questions'
+            linkText= 'Ask a Question'
+          />
+        }
       </div>
     </>
   );
